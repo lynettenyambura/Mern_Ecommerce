35f155370f9d84f317c9c9d50c39ec2740e565b6
lynettenyambura <linetnyambura45@gmail.com>
2024-04-04 21:02:53 +0300

    model integration

--- frontend/src/components/review/ListReviews.js
+++ frontend/src/components/review/ListReviews.js
@@ -1,25 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const ListReviews = ({ reviews }) => {
+    // Function to get class based on sentiment
+    const getSentimentClass = (sentiment) => {
+        if (sentiment === 'positive') {
+            return 'positive-sentiment';
+        } else if (sentiment === 'neutral') {
+            return 'neutral-sentiment';
+        } else {
+            return 'negative-sentiment';
+        }
+    };
+
     return (
-        <div class="reviews w-75">
+        <div className="reviews w-75">
             <h3>Other's Reviews:</h3>
             <hr />
             {reviews && reviews.map(review => (
-                <div key={review._id} class="review-card my-3">
-                    <div class="rating-outer">
-                        <div class="rating-inner" style={{ width: `${(review.rating / 5) * 100}%` }}></div>
+                <div key={review._id} className="review-card my-3">
+                    <div className="rating-outer">
+                        <div className="rating-inner" style={{ width: `${(review.rating / 5) * 100}%` }}></div>
                     </div>
-                    <p class="review_user">by {review.name}</p>
-                    <p class="review_comment">{review.comment}<i>{review.sentiment} </i></p>
-                    
-
-
+                    <p className="review_user">by {review.name}</p>
+                    <p className="review_comment">
+                        {review.comment}<span className={`sentiment ${getSentimentClass(review.sentiment)}`}>{review.sentiment}</span>
+                    </p>
                     <hr />
                 </div>
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default ListReviews
+export default ListReviews;
